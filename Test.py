2e87e29545fba2a53afc76e6a31f40c3a96d67ed
Eliza <eberman@groupninemedia.com>
2018-08-20 13:47:39 -0400

    second commit for branch change

--- Test.py
+++ Test.py
@@ -2,27 +2,29 @@ from Musician import *
 from DataSet import *
 import csv
 
-##def CountQuestions(musician, ds):
-def CountQuestions(musician):
+def CountQuestions(MusicianNum):
     ##pass
-    name = DS.MusicianList[musician].Name
+    ds = DataSet(musicians)
+    name = ds.MusicianList[MusicianNum].Name
     Mnumber = 0
     QCount = 0
-    while DS.ZeroCount < 9: 
-        for x in range(len(DS.MusicianList)):
-            if name == DS.MusicianList[x].Name:
+    MCount = 0
+    TotalCount = 0
+    NamesList = []
+    
+    while ds.ZeroCount < 9: 
+        for x in range(len(ds.MusicianList)):
+            if name == ds.MusicianList[x].Name:
                 Mnumber = x    
-        if DS.MusicianList[Mnumber].Characteristics[DS.FrequentTopic] == "1":
-            DS.update("Yes")
+        if ds.MusicianList[Mnumber].Characteristics[ds.FrequentTopic] == "1":
+            ds.update("Yes")
             QCount += 1
         else :
-            DS.update("No")
-            QCount += 1
-    MCount = 0           
-    DS.MusicianList.sort(key=lambda MusicianGuess: MusicianGuess.Frequency, reverse = True)
-    NamesList = []
-    for l in range(len(DS.MusicianList)):
-        NamesList.append(DS.MusicianList[l].Name)
+            ds.update("No")
+            QCount += 1          
+    ds.MusicianList.sort(key=lambda MusicianGuess: MusicianGuess.Frequency, reverse = True)
+    for l in range(len(ds.MusicianList)):
+        NamesList.append(ds.MusicianList[l].Name)
     MCount = NamesList.index(name)
     TotalCount = MCount + QCount + 1
     return TotalCount;
@@ -30,8 +32,9 @@ def CountQuestions(musician):
 def RunTest(DS):
     Questions = []
     for x in range(len(DS.MusicianList)):
-##        Questions.append(CountQuestions(DS.MusicianList[x], DS))
-##        Questions.append(CountQuestions(x))
+        #print(x)
+        Questions.append(CountQuestions(x))
+        #Questions.append(CountQuestions(x, DS))
         print(DS.MusicianList[x].Name, CountQuestions(x))
 
 if __name__ == "__main__":
@@ -42,5 +45,4 @@ if __name__ == "__main__":
             RowCount = RowCount + 1
             musicians.append(Musician(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8],row[9], row[10], row[11]))
     DS = DataSet(musicians)
-
     RunTest(DS)
